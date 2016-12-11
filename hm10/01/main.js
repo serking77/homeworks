2d10d7d0b21b11e116198bc32f926b88a5dc0c55
Valery Bagaev <valery.bagaev84@gmail.com>
2016-12-11 17:51:16 +0400

    добавил конструктор

--- hm10/01/main.js
+++ hm10/01/main.js
@@ -8,82 +8,21 @@ calculator.prototype.sum = function() {
             summArg += arguments[i];
         }
         return this.firstNumber + summArg;
-    }
-
-/*    this.sum = function() {
-        var summArg = 0;
-        for (var i = 0; i < arguments.length; i++) {
-            summArg += arguments[i];
-        }
-        return firstNumber + summArg;
-    }
-*/
-
-
-
-
-
-
-
-var obj = new calculator(100);
-console.dir(obj);
-
-console.log(obj.sum(1, 2, 3));
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*Написать функцию 'calculator', которая имеет один параметр - 'firstNumber'
-- 'sum' - складывает 'firstNumber' с переданным аргументами
-- 'dif' - вычитает из 'firstNumber' переданные аргументы
-- 'div' - делит 'firstNumber' на первый переданный аргумент. Результат этой операции делится на второй переданный аргумент (если он есть) и так далее
-- 'mul' - умножает 'firstNumber' на первый переданный аргумент. Результат этой операции умножается на второй переданный аргумент (если он есть) и так далее.
-Предусмотреть исключительные ситуации, для функции 'div', когда делитель равен нулю
-пример:
-var myCalculator = calculator(100);
-
-console.log(myCalculator.sum(1, 2, 3)); //вернет 106
-
-console.log(myCalculator.dif(10, 20)); //вернет 70
-
-console.log(myCalculator.div(2, 2)); //вернет 25
-
-console.log(myCalculator.mul(2, 2)); //вернет 400*/
-
-/*function calculator(firstNumber) {
-
-    var obj = {
-
-        sum: function() {
-            var summArg = 0;
-            for (var i = 0; i < arguments.length; i++) {
-                summArg += arguments[i];
-            }
-            return firstNumber + summArg;
-        },
-        dif: function() {
+    };
+calculator.prototype.dif = function() {
             var difArg = 0;
             for (var i = 0; i < arguments.length; i++) {
                 difArg += arguments[i];
             }
-            return firstNumber - difArg;
+            return this.firstNumber - difArg;
 
-        },
-        div: function() {
+        };
+calculator.prototype.div = function() {
 
             try {
 
                 if (arguments[0] !== 0) {
-                    var result = firstNumber / arguments[0];
+                    var result = this.firstNumber / arguments[0];
                 } else {
                     throw new Error("Делить на ноль нельзя!")
                 }
@@ -101,25 +40,25 @@ console.log(myCalculator.mul(2, 2)); //вернет 400*/
             } catch (err) {
                 return err.message;
             }
-        },
+        };
 
-        mul: function() {
-            var result = firstNumber * arguments[0];
+calculator.prototype.mul = function() {
+            var result = this.firstNumber * arguments[0];
 
             for (var i = 1; i < arguments.length; i++) {
                 result = result * arguments[i]
             }
             return result;
-        },
+        };
 
-    }
-    return obj;
-}
+var myCalculator = new calculator(100);
 
-var myCalculator = calculator(100);
-console.log(calculator(100));
-console.log(myCalculator.sum(1, 2, 3));
-console.log(myCalculator.dif(10, 20));
-console.log(myCalculator.div(2, 2));
-console.log(myCalculator.mul(2, 2));
-*/
+console.log(myCalculator.sum(1, 2, 3)); //вернет 106
+
+console.log(myCalculator.dif(10, 20)); //вернет 70
+
+console.log(myCalculator.div(2, 2)); //вернет 25
+
+console.log(myCalculator.mul(2, 2)); //вернет 400*/
+
+
