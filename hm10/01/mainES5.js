4e07d38b76eb719c342ce71d0e38f543f3eb79ae
Valery Bagaev <valery.bagaev84@gmail.com>
2016-12-19 19:45:38 +0400

    Исправил недочеты

--- hm10/01/mainES5.js
+++ hm10/01/mainES5.js
@@ -1,66 +1,65 @@
-
+"use strict";
 var calculator = function(firstNumber) {
     this.firstNumber = firstNumber;
-}
-
+};
 calculator.prototype.sum = function() {
-        var summArg = 0;
-        for (var i = 0; i < arguments.length; i++) {
-            summArg += arguments[i];
-        }
-        return this.firstNumber + summArg;
-    };
+    var summArg = 0;
+    for (var i = 0; i < arguments.length; i++) {
+        summArg += arguments[i];
+    }
+    return this.firstNumber + summArg;
+};
 calculator.prototype.dif = function() {
-            var difArg = 0;
-            for (var i = 0; i < arguments.length; i++) {
-                difArg += arguments[i];
-            }
-            return this.firstNumber - difArg;
+    var difArg = 0;
+    for (var i = 0; i < arguments.length; i++) {
+        difArg += arguments[i];
+    }
+    return this.firstNumber - difArg;
 
-        };
+};
 calculator.prototype.div = function() {
+    var result;
+    try {
 
-            try {
+        if (arguments[0] !== 0) {
+            result = this.firstNumber / arguments[0];
+        } else {
+            throw new Error("Делить на ноль нельзя!");
+        }
 
-                if (arguments[0] !== 0) {
-                    var result = this.firstNumber / arguments[0];
-                } else {
-                    throw new Error("Делить на ноль нельзя!")
-                }
+        for (var i = 1; i < arguments.length; i++) {
 
-                for (var i = 1; i < arguments.length; i++) {
-
-                    if (arguments[i] !== 0) {
-                        result = result / arguments[i]
-                    } else {
-                        throw new Error("Делить на ноль нельзя!")
-                    }
-
-                }
-                return result;s
-            } catch (err) {
-                return err.message;
+            if (arguments[i] !== 0) {
+                result = result / arguments[i];
+            } else {
+                throw new Error("Делить на ноль нельзя!");
             }
-        };
+
+        }
+        return result;
+    } catch (err) {
+        return err.message;
+    }
+};
 
 calculator.prototype.mul = function() {
-            var result = this.firstNumber * arguments[0];
+    var result = this.firstNumber * arguments[0];
 
-            for (var i = 1; i < arguments.length; i++) {
-                result = result * arguments[i]
-            }
-            return result;
-        };
+    for (var i = 1; i < arguments.length; i++) {
+        result = result * arguments[i];
+    }
+    return result;
+};
 
 
 var mySqlCalc = function(firstNumber) {
     this.firstNumber = firstNumber;
-}
+};
 
 function inherit(child, parent) {
- child.prototype = Object.create(parent.prototype);
- child.prototype.constructor = child;
- child.prototype.parent = parent;
+    child.prototype = Object.create(parent.prototype);
+    child.prototype.constructor = child;
+    child.prototype.parent = parent;
 }
 
 inherit(mySqlCalc, calculator);
@@ -68,24 +67,24 @@ inherit(mySqlCalc, calculator);
 
 mySqlCalc.prototype.sum = function() {
     var calc2 = this.parent.prototype.sum.apply(this, arguments);
-        return calc2 * calc2;
+    return calc2 * calc2;
 };
 mySqlCalc.prototype.dif = function() {
     var calc2 = this.parent.prototype.dif.apply(this, arguments);
-        return calc2 * calc2;
+    return calc2 * calc2;
 };
 mySqlCalc.prototype.div = function() {
     var calc2 = this.parent.prototype.div.apply(this, arguments);
-        return calc2 * calc2;
+    return calc2 * calc2;
 };
 mySqlCalc.prototype.mul = function() {
     var calc2 = this.parent.prototype.mul.apply(this, arguments);
-        return calc2 * calc2;
+    return calc2 * calc2;
 };
 
-var myCalculator = new calculator(100);
+new calculator(100);
 var SqlCalc = new mySqlCalc(100);
 console.log(SqlCalc.sum(1, 2, 3)); //вернет 11 236 (100 + 1 + 2 + 3 = 106 * 106)
 console.log(SqlCalc.dif(10, 20)); //вернет 4 900
 console.log(SqlCalc.div(2, 2)); //вернет 625
-console.log(SqlCalc.mul(2, 2)); //вернет 160 000 
+console.log(SqlCalc.mul(2, 2)); //вернет 160 000
--- hm10/01/mainES6.js
+++ hm10/01/mainES6.js
@@ -1,63 +1,64 @@
+"use strict";
 class calculator {
     constructor(firstNumber) {
         this.firstNumber = firstNumber;
     }
     sum() {
-        var summArg = 0;
-        for (var i = 0; i < arguments.length; i++) {
+        let summArg = 0;
+        for (let i = 0; i < arguments.length; i++) {
             summArg += arguments[i];
         }
         return this.firstNumber + summArg;
     }
     dif() {
-        var difArg = 0;
-        for (var i = 0; i < arguments.length; i++) {
+        let difArg = 0;
+        for (let i = 0; i < arguments.length; i++) {
             difArg += arguments[i];
         }
         return this.firstNumber - difArg;
 
     }
     div() {
-
+        let result;
         try {
 
             if (arguments[0] !== 0) {
-                var result = this.firstNumber / arguments[0];
+                result = this.firstNumber / arguments[0];
             } else {
-                throw new Error("Делить на ноль нельзя!")
+                throw new Error("Делить на ноль нельзя!");
             }
 
-            for (var i = 1; i < arguments.length; i++) {
+            for (let i = 1; i < arguments.length; i++) {
 
                 if (arguments[i] !== 0) {
-                    result = result / arguments[i]
+                    result = result / arguments[i];
                 } else {
-                    throw new Error("Делить на ноль нельзя!")
+                    throw new Error("Делить на ноль нельзя!");
                 }
 
             }
             return result;
-            s
+            
         } catch (err) {
             return err.message;
         }
     }
 
     mul() {
-        var result = this.firstNumber * arguments[0];
+        let result = this.firstNumber * arguments[0];
 
-        for (var i = 1; i < arguments.length; i++) {
-            result = result * arguments[i]
+        for (let i = 1; i < arguments.length; i++) {
+            result = result * arguments[i];
         }
         return result;
     }
 }
 
-var obj = new calculator(100);
-console.log(obj.sum(1, 2, 3)); //вернет 11 236 (100 + 1 + 2 + 3 = 106 * 106)
-console.log(obj.dif(10, 20)); //вернет 4 900
-console.log(obj.div(2, 2)); //вернет 625
-console.log(obj.mul(2, 2)); //вернет 160 000
+let obj = new calculator(100);
+console.log(obj.sum(1, 2, 3)); 
+console.log(obj.dif(10, 20)); 
+console.log(obj.div(2, 2)); 
+console.log(obj.mul(2, 2)); 
 
 
 
@@ -67,26 +68,26 @@ class mySqlCalc extends calculator {
     }
 
     sum() {
-        var calc2 = super.sum.apply(this, arguments);
+        let calc2 = super.sum.apply(this, arguments);
         return calc2 * calc2;
     }
     dif() {
-        var calc2 = super.dif.apply(this, arguments);
+        let calc2 = super.dif.apply(this, arguments);
         return calc2 * calc2;
     }
     div() {
-        var calc2 = super.div.apply(this, arguments);
+        let calc2 = super.div.apply(this, arguments);
         return calc2 * calc2;
     }
     mul() {
-        var calc2 = super.mul.apply(this, arguments);
+        let calc2 = super.mul.apply(this, arguments);
         return calc2 * calc2;
     }
 
 }
 
-var SqlCalc = new mySqlCalc(100);
+let SqlCalc = new mySqlCalc(100);
 console.log(SqlCalc.sum(1, 2, 3)); //вернет 11 236 (100 + 1 + 2 + 3 = 106 * 106)
 console.log(SqlCalc.dif(10, 20)); //вернет 4 900
 console.log(SqlCalc.div(2, 2)); //вернет 625
-console.log(SqlCalc.mul(2, 2)); //вернет 160 000 
+console.log(SqlCalc.mul(2, 2)); //вернет 160 000
